d7178f1be12f4f6c2910e539d0e6225e9917fce4
shaikswabirachand <skswabirachand@yahoo.com>
2025-02-18 21:38:58 +0530

    MissingNumber.cpp

---
+++ deepaCPP/MissingNumber.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter a number: ";
+    int n;
+    cin>>n;
+    cout<<"Enter "<<n<<" numbers";
+    int a[n];
+
+
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+    }
+
+    // int arr[7] = {0,1,2,3,4,5,7};
+    // int b[7] = {0,1,2,3,4,5,6};
+    int l = 0,r = 6,mid = (r+l)/2;
+    while(true){
+        if(arr[mid] == b[mid]){
+            l = mid;
+        }else{
+            r = mid;
+        }
+        mid = (r+l)/2;
+
+        if(r-l == 1){
+            if(arr[r] == b[r]){
+                cout<<"no number is missing";
+            }
+            else{
+                cout<<r;
+            }
+
+            break;
+        }
+    }
+    
+
+}
