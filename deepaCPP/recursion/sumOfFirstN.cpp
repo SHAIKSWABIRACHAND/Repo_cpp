8f982ba21dd2685bf34185306a5c3b2b3d2459b2
shaikswabirachand <skswabirachand@yahoo.com>
2025-02-22 18:05:42 +0530

    series program

--- deepaCPP/recursion/sumOfFirstN.cpp
+++ deepaCPP/recursion/sumOfFirstN.cpp
@@ -7,3 +7,27 @@ Enter the common difference (d): 1
 Enter the number of terms (n): 5                                                                                        
 Sum of the first 5 terms of the arithmetic series is: 15
 */
+
+#include<iostream>
+using namespace std;
+int Series(int a,int d,int n){
+    if(n==0){
+        return 0;
+    }
+    int num = a+(n-1)*d;
+    return num+Series(a,d,n-1);
+}
+
+int main(){
+    cout<<"Enter a: ";
+    int a;
+    cin>>a;
+    cout<<"Enter d ";
+    int d;
+    cin>>d;;
+    cout<<"Enter n: ";
+    int n;
+    cin>>n;
+
+    cout<<Series(a,d,n);
+}
